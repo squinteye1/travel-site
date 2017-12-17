daa926c2d265f5fd76c3bdae7ca79adbae3f535d
Neil <neil.d.mckay@gmail.com>
2017-12-17 23:43:08 +0000

    using gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,9 @@
 var gulp = require('gulp'),
-watch = require('gulp-watch');
-postcss = require('gulp-postcss');
+watch = require('gulp-watch'),
+postcss = require('gulp-postcss'),
+autoprefixer = require('autoprefixer'),
+cssvars = require('postcss-simple-vars'),
+nested = require('postcss-nested');
 
 gulp.task('default', function() {
   console.log("Hooray - you created a Gulp task.");
@@ -12,8 +15,8 @@ gulp.task('html', function() {
 
 gulp.task('styles', function() {
   return gulp.src('./app/assets/styles/styles.css')
-  .pipe(postcss())
-  .pipe(gulp.dest('.app/temp/styles'));
+  .pipe(postcss([nested, cssvars, autoprefixer]))
+  .pipe(gulp.dest('./app/temp/styles'));
 });
 
 gulp.task('watch', function() {

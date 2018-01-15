544f5ce305f68621a95de30fa4c8d901a48818ad
Neil <neil.d.mckay@gmail.com>
2018-01-15 23:24:14 +0000

    installed webpack

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -16,18 +16,13 @@ john.greet();
 person(john.name, john.favColour);
 person('Jane Smith','green');
 */
-
-function Person (fullName, favColour) {
-    this.name = fullName;
-    this.favoriteColour = favColour;
-    this.greet = function () {
-    this.favoriteColour = favColour;
-        console.log('Hello my name is ' + this.name + ' and my favorite colour is ' + this.favoriteColour + '.');
-    }
-}
+var $ = require('jquery');
+var Person = require('./modules/Person');
 
 var john = new Person("John Doe","blue");
 john.greet();
 
 var jane = new Person("Jane Smith","green");
 jane.greet();
+
+$('h1').remove();
